c2d54173987cc6634857ff1cf0f1764acb673c54
Piotr Dobrowolski <dobrypd@gmail.com>
2013-01-10 21:57:44 +0100

    proper test script

--- scripts/test_runner.sh
+++ scripts/test_runner.sh
@@ -5,7 +5,7 @@ echo "Run only from scripts directory"
 # FLAG, 1 if you dont want program outputs
 DELETE_OUTPUTS=1
 # FLAG IF want Latte to show debug informations
-DEBUG_OUT=1
+DEBUG_OUT=0
 
 
 PROJECT_DIR=`pwd`/../
@@ -19,30 +19,39 @@ do
     echo -en "\033[38m\033[32mLatte runned on file : \033[0m"$i"\n"
     if [ $DEBUG_OUT == 0 ]
     then
-        $LATTE $i 2> /dev/null 1 > $i.output.PROUT
+        $LATTE $i 2> /dev/null
     else
-        $LATTE $i 1> $i.output.PROUT
+        $LATTE $i
     fi
-done
-
-#check
-
-for i in `find $TESTS_IN_DIR -iname "*.output"`
-do
+    
+    BN=$(basename $i)
+    DN=$(dirname $i)
+    
+    echo "$DN/a.out > $i.PROUT"
+    $DN/a.out > $i.PROUT
+    
     echo -en "\033[38m\033[33mChecking outputs for: \033[0m"$i"\n"
-    if diff $i.PROUT $i
+    OUT_FN="${i%.*}".output
+    if [ ! -f $i.PROUT ] || [ ! -f $OUT_FN ]
     then
-        echo -en "\033[38m\033[32mOK\033[0m\n"
+        echo -en "\033[38m\033[31mBAD (file not found) [$BN]\033[0m\n"
     else
-        echo -en "\033[38m\033[31mBAD\033[0m\n"
+        if diff $i.PROUT $OUT_FN
+        then
+            echo -en "\033[38m\033[32mOK [$BN]\033[0m\n"
+        else
+            echo -en "\033[38m\033[31mBAD (outputs differs) [$BN]\033[0m\n"
+        fi
     fi
+    
+    rm $DN/a.out
 done
+
 
 #delete prog outputs
 if [ $DELETE_OUTPUTS == 1 ]
 then
-    for i in `dir -d *PROUT`
-    do
-        rm $i
-    done
+    find $TESTS_IN_DIR -iname "*.PROUT" | xargs rm
+    find $TESTS_IN_DIR -iname "*.s" | xargs rm
+    find $TESTS_IN_DIR -iname "*.out" | xargs rm
 fi
